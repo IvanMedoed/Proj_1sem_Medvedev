012376005cfeef0ce80e416626e58c87a9b6426c
cherrra <annna.cherrr@yandex.ru>
2022-11-14 16:57:39 +0300

    изменено PZ_6_2(недоработано) добавлено PZ_6_3

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -1,25 +1,23 @@
 # Дан список размера N. Найти номера двух ближайших элементов из этого списка( то есть элементов с наименьшем модулем
 # разности) и вывести эти номера в порядке возрастания
 import random
-N = []
-c=0
-b = int(input('Введите размер списка  =>'))
-while c < b:
-  N.append(random.randint(0,b))
-  c +=1
 
-o_min = abs(N[0] - N[1])
+N = int(input('Введите размер списка  =>'))
+a = [random.randrange(1,100) for i in range(N)]
+
+print('Spisok', a)
+o_min = abs(a[0] - a[1])
 i_min = 0
 j_min = 1
 for i in range(0, N-1):
   for j in range(i+1, N):
-    b_tmp = abs(N[i] - N[j])
+    b_tmp = abs(a[i] - a[j])
     if o_min > b_tmp:
       o_min = b_tmp
       i_min = i
       j_min = j
 print(o_min)
-print(N[i_min], '', N[j_min])
+print(i_min, ',', j_min)
 
 
 
