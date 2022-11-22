6f4118f7bb72542fad08810132d648d76ff0ae2c
IvanMedoed <shockland0@yandex.ru>
2022-11-22 10:08:10 +0300

    изменено PZ_6_2

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -5,19 +5,18 @@ import random
 N = int(input('Введите размер списка  => '))
 a = [random.randrange(1,100) for i in range(N)]
 
-print('Spisok', a)
-o_min = abs(a[0] - a[1])
+
+o_min = abs(a[0] - a[1]) # модуль разности нулевого и первого индекса
 i_min = 0
 j_min = 1
 for i in range(0, N-1):
   for j in range(i+1, N):
-    b_tmp = abs(a[i] - a[j])
+    b_tmp = abs(a[i] - a[j]) #b_tmp модуль разности между двумя ближайшими элементами из списка
     if o_min > b_tmp:
       o_min = b_tmp
       i_min = i
       j_min = j
-print(o_min)
-print(i_min, ',', j_min)
+print('Номера элементов с наименьшем модулем разности:',i_min, ',', j_min)
 
 
 
