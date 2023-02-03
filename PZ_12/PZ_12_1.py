f9e10c89262c5f935f84b44e5e09b9982faca336
IvanMedoed <shockland0@yandex.ru>
2023-02-03 13:31:45 +0300

    PZ_12_1.py изменено

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -1,9 +1,10 @@
 #В последовательности на  n целых чисел умножить
+#  все элементы на последний минимальный элемент.
 
 a = list(map(int, input().split()))
 min=a[0]
 i_min=0
-for i in range (len(a)):
+for i in range (len(a)): #Цикл находит последний минимальный элемент массива
     if a[i]<min:
         i_min=i
-print([x * a[i_min] for x in a])
+print([x * a[i_min] for x in a]) #Умножение всех элементов на последний минимальный
