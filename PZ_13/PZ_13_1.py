0e7166dcebc08e155b97a484ef26424dad07d338
IvanMedoed <shockland0@yandex.ru>
2023-02-04 16:26:42 +0300

    PZ_13_2 доделать

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -1,8 +1,14 @@
 #Сгенерировать матрицу на произвольное количество элементов, в которой задается преобразование от предыдущего элемента
 # к следующему на произвольное значение
+from functools import reduce
 import random
 mat = [[], []]
-
+c = 0
 for i in range(0, random.randint(3, 35)):
     mat[0].append(random.randint(0, 39))
-    mat[1].append(random.randint(0, 40))
+    y = random.randint(0,39)
+    print(y)
+    b = reduce(lambda x, y: x + y, mat[0])
+    mat[1].append(b)
+
+print(mat)
