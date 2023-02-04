6f364b12c1af280653b325e8a9fe86f87b805b82
IvanMedoed <shockland0@yandex.ru>
2023-02-04 16:26:58 +0300

    PZ_13_1 доделать

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -8,7 +8,7 @@ for i in range(0, random.randint(3, 35)):
     mat[0].append(random.randint(0, 39))
     y = random.randint(0,39)
     print(y)
-    b = reduce(lambda x, y: x + y, mat[0])
+    b = reduce(lambda x,y: x + y, mat[0])
     mat[1].append(b)
 
 print(mat)
