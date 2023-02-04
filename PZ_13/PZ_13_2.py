b968679d632efa0841dc7f324df7de5fc1f7cd65
IvanMedoed <shockland0@yandex.ru>
2023-02-04 16:13:11 +0300

    PZ_13_2 изменено

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -1,1 +1,14 @@
 #В матрице найти сумму элементов первых строк
+import random
+from functools import reduce
+
+
+mat = [[], []]
+
+for i in range(0, random.randint(3, 35)):
+    mat[0].append(random.randint(0, 39))
+    mat[1].append(random.randint(0, 40))
+
+print(mat)
+pin = reduce(lambda x, y: x + y, mat[0])
+print(pin)
