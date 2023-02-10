7b626416eb11b3af55fc67a62d566791f7e75541
cherrra <annna.cherrr@yandex.ru>
2023-02-10 14:33:01 +0300

    PZ_13_1 изменено

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -8,8 +8,8 @@ for i in range(0, random.randint(0, 35)):
     y.append(random.randint(0, 35))
     c = map(sum, zip(mat[0], y)) # первая строка матрицы суммируется с рандомными элементами
 mat[1].append(list(c)) # Эти значения добавляет во вторую строку матрицу
-print(mat[0])
+print(mat[0]) #первая строка матрицы
 print(y)
-print(mat)
+print(mat) # вся матрица
 
 
