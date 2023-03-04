3c1b8e63746e83f41babb2743ee395e43078fad2
IvanMedoed <shockland0@yandex.ru>
2023-03-04 15:27:40 +0300

    PZ_14_1 ИЗМЕНЕНО

--- PZ_14/PZ_14_1.py
+++ PZ_14/PZ_14_1.py
@@ -1,7 +1,6 @@
 #Из исходного текстового файла (experience.txt) выбрать стаж работы. Посчитать количество полученных элементов
 
 import re
-
 file = open('experience.txt')
 values = file.read().split("\n")
 data = []
@@ -9,18 +8,11 @@ for key in values:
     value = re.findall(r"[-+]?\.\d+|\d+", key) # получаем все возможные цифры с текствого файла
     if value != []:
         data.append(value)
-
-data_new = [list(map(int, x)) for x in data] #переводим матрицу в тип инт
-data_new[8].append(0)
-print(data_new) #матрица исходных данных стажа работы
-
-month = 0
-year = 0
-for i in data_new:
-    month += i[1]
-    year += i[0]
-
-print(f'Стаж работы: {year} лет, {month} месяцев')
+def changing(matrix):   #изменяем матрицу в массив, чтобы получить точное кол-во всех элементов
+    arr = [elem for el in matrix for elem in el]
+    print(len(arr))
+print(data)
+changing(f'Всего элементов: {data}')
 
 
 
