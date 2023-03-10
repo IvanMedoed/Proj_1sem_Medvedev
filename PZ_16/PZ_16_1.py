d43248164749d9cd86977ed4dfb2d99b193a1fa8
IvanMedoed <shockland0@yandex.ru>
2023-03-10 15:52:11 +0300

    PZ_16_1 Добавлено

---
+++ PZ_16/PZ_16_1.py
@@ -0,0 +1,3 @@
+from figures import circlePerimeter, circleArea, squarePerimeter, squareArea, trianglePerimeter, triangleArea
+
+print(circlePerimeter(40),'\n', circleArea(20),'\n', squarePerimeter(12),'\n', squareArea(27),'\n', trianglePerimeter(15),'\n', triangleArea(33))
