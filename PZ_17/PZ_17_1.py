0846fb37b617e679a3da5613fd4a0860bf3d53aa
IvanMedoed <shockland0@yandex.ru>
2023-06-01 11:11:39 +0300

    PZ_17.py изменено

--- PZ_17/PZ_17_1.py
+++ PZ_17/PZ_17_1.py
@@ -1,2 +1,24 @@
 #Создайте класс "Матрица", который имеет атрибуты количества строк и столбцов.
 #Добавьте методы для вычисления процентных начислений и снятия денег
+class Matrix:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+
+    def percent_increase(self, amount, percentage):
+        increase = amount * percentage / 100
+        return amount + increase
+
+    def withdraw_money(self, amount):
+        if amount > 0:
+            print(f"Снятие {amount}  долларов")
+        else:
+            print("Введите цифры")
+
+
+# Пример использования класса:
+
+account_balance = Matrix(1, 1)
+print(account_balance.percent_increase(100, 10))  # Выведет 110.0
+account_balance.withdraw_money(50)  # Выведет "Вывод 50 долларов"
+account_balance.withdraw_money(-10)  # Выведет "Пожалуйста, введите действительную сумму"
--- PZ_17/PZ_17_2.py
+++ PZ_17/PZ_17_2.py
@@ -1,2 +1,57 @@
 #Создание базового класса "Фигура" и его наследование для создания классов "Квадрат", "Прямоугольник" и "Круг". Класс "Фигура"
 # будет иметь общие методы, такие как вычисление площади и периметра, а классы наследники будут иметь свои уникальные свойства и методы
+
+import math
+
+class Figure:
+    def __init__(self):
+        pass
+
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+class Square(Figure):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+    def perimeter(self):
+        return 4 * self.side
+
+class Rectangle(Figure):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+class Circle(Figure):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+s = Square(5)
+print(s.area())  # выведет 25
+print(s.perimeter())  # выведет 20
+
+r = Rectangle(4, 5)
+print(r.area())  # выведет 20
+print(r.perimeter())  # выведет 18
+
+c = Circle(3)
+print(c.area())  # выведет приблизительно 28.27
+print(c.perimeter())  # выведет приблизительно 18.85
